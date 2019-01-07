aef76b35e093f6f5e8c92bfce0eabc5d1f88c798
sjihdazi <sjihdazi@gmail.com>
2019-01-07 21:02:47 +0100

    dfgdf

---
+++ periode 2/php/eidopdracht/index.php
@@ -0,0 +1,14 @@
+<?php
+	include "db.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>nhlstenden twitter</title>
+	<link rel="stylesheet" type="text/css" href="css/materialize.css">
+	<script type="text/javascript"></script>
+</head>
+<body>
+
+</body>
+</html>
