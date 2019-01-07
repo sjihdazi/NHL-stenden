2a76108cb7daf3308c4d52445c2bfdf9bc597f39
sjihdazi <sjihdazi@gmail.com>
2019-01-07 12:30:38 +0100

    Z

--- periode 2/php/week 4-5/opdr 3/index.php
+++ periode 2/php/week 4-5/opdr 3/index.php
@@ -42,35 +42,6 @@ require('config.php');
 		} else {
 			echo "<p>Entries are NOT found.</p>";
 		}
-		// if ($stmt = mysqli_prepare($conn, $select)) {
-		// 	mysqli_stmt_execute($stmt);
-
-		// 	if ($mysqli_stmt_num_rows($stmt) == 0) {
-		// 		echo "<p>There are no entries</p>";
-		// 	}
-		// } else {
-		// 	echo "<p>Entries ARE FOUND.</p>";
-			// echo "Dit zijn alle bugs:</p>";
-			// echo "<table width='100%' border='1'>";
-			// echo "<tr>
-			// <th>Name</th>
-			// <th>Version</th>
-			// <th>Hardware</th>
-			// <th>OS</th>
-			// <th>Frequency / day</th>
-			// <th>Solution</th>
-			// </tr>";
-			// while ($row = mysqli_stmt_fetch($stmt)) {
-			// 	echo "<tr>
-			// 	<td>" . $row["product_name"] . "</td>
-			// 	<td>" . $row["version"] . "</td>
-			// 	<td>" . $row["hardware"] . "</td>
-			// 	<td>" . $row["os"] . "</td>
-			// 	<td>" . $row["frequency"] . "</td>
-			// 	<td>" . $row["solution"] . "</td>
-			// 	</tr>";
-			// }
-			//}
 
 		mysqli_close($conn);
 		?>
